2cd771fa251f02d5a35bb84141e9a410c08e34bd
vinicius-rover <165529227+vinicius-rover@users.noreply.github.com>
2024-04-16 00:59:03 -0300

    Implementing possible moves of Rook and King and printing possible moves

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,7 +8,7 @@ public class Board {
 
 	public Board(int rows, int columns) {
 		if (rows < 1 || columns < 1) {
-			throw new BoardException("Error creating board: there must be at lesat 1 row and 1 column");
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
 		}
 		this.rows = rows;
 		this.columns = columns;
@@ -18,17 +18,20 @@ public class Board {
 	public int getRows() {
 		return rows;
 	}
-	
+
 	public int getColumns() {
 		return columns;
 	}
 
 	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 	}
 
 	public Piece piece(Position position) {
-		if (!positionExistis(position)) {
+		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
 		return pieces[position.getRow()][position.getColumn()];
@@ -36,36 +39,35 @@ public class Board {
 
 	public void placePiece(Piece piece, Position position) {
 		if (thereIsAPiece(position)) {
-			throw new BoardException("There is already a piece on position");
+			throw new BoardException("There is already a piece on position " + position);
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
-	
+
 	public Piece removePiece(Position position) {
-		if (!positionExistis(position)) {
+		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
-		if(piece(position) == null) {
+		if (piece(position) == null) {
 			return null;
 		}
 		Piece aux = piece(position);
-		aux.position=null;
+		aux.position = null;
 		pieces[position.getRow()][position.getColumn()] = null;
 		return aux;
 	}
 
-
-	private boolean positionExistis(int row , int column) {
-		return	row >= 0 && row < rows && column >= 0 && column < columns;
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
 
-	public boolean positionExistis(Position position) {
-		return positionExistis(position.getRow(), position.getColumn());
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
 	}
-	
+
 	public boolean thereIsAPiece(Position position) {
-		if (!positionExistis(position)) {
+		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
 		return piece(position) != null;
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,10 +25,17 @@ public class ChessMatch {
 		return mat;
 	}
 
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
 	}
@@ -37,18 +44,24 @@ public class ChessMatch {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
-		return (ChessPiece) capturedPiece;
+		return capturedPiece;
 	}
 
 	private void validateSourcePosition(Position position) {
-		if (!board.thereIsAPiece(position)){
+		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
 		if (!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for the chosen piece");
 		}
 	}
-	
+
+	private void validateTargetPosition(Position source, Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to target position");
+		}
+	}
+
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
@@ -68,5 +81,4 @@ public class ChessMatch {
 		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
-
 }
